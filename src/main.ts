f05b3d1e9d178d299807bb336f63042c79f67c35
Jayden Lee <41176085+tkxkd0159@users.noreply.github.com>
2023-03-24 23:01:26 +0900

    set gh-action workflow

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,47 @@
 import { Octokit } from "@octokit/rest";
 import { RequestError } from "@octokit/request-error";
+import { createAppAuth } from "@octokit/auth-app";
 import * as dotenv from 'dotenv'
 dotenv.config()
 import {prompt} from "./lib"
 
-const client = new Octokit({
-    auth: "",
-    baseUrl: "https://api.github.com",
-});
-
-void (function() {
+void (async function() {
     try {
         let owner, repo, tag: string | undefined;
         if (typeof process.env.GITHUB_REPOSITORY === 'string') {
             [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
         }
-        if (typeof process.env.GITHUB_REF === 'string') {
+        if (typeof process.env.GITHUB_REF === 'string') { // refs/tags/<tag_name> OR refs/pull/<pr_number>/merge
             tag = process.env.GITHUB_REF.split("/")[2]
         }
         if (owner === undefined || repo === undefined || tag === undefined) {
             process.exit(1);
         }
 
-        console.log(prompt({
-            owner,
-            repo,
-            tag,
-        },"I am msg"))
+        const client = new Octokit({
+            authStrategy: createAppAuth,
+            auth: {
+                appId: 309530,
+                privateKey: process.env.AUTOPR_APP_TOKEN,
+                installationId: 35627228,
+            },
+            baseUrl: "https://api.github.com",
+        });
 
-        // const prs = await client.rest.pulls.create()
-        // console.log(prs)
+        await client.rest.pulls.create(
+            {
+                owner: "tkxkd0159",
+                repo: "autopr",
+                title: "autopr test",
+                head: "octopr-test",
+                base: "main",
+                body: prompt({
+                    owner,
+                    repo,
+                    tag,
+                },"<!--- Describe your changes in detail -->\r\n"+"I am msg\r\n")
+            }
+        )
 
     } catch (e) {
         if (e instanceof RequestError) {
